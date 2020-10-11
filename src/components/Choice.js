19a67b41de04e78648b0a8342c82d927034adec1
Piyopanman <g19911kh@gm.tsuda.ac.jp>
2020-10-11 10:19:30 +0900

    modify quiz

--- src/components/Choice.js
+++ src/components/Choice.js
@@ -22,10 +22,27 @@ const Choice = (props) =>{
                 onClick = {()=> changeAnswer()}
             >
                 <div className="card-action">
-                    
+                    <div className="row no-margin">
+                        <span className="col s11 choice-content">{choice}</span>
+                        <span className="col s1 checkbox-container">
+                            <label>
+                                <input 
+                                    type="checkbox"
+                                    onClick={(e) => e.stopPropagatio()}
+                                    checked={checked}
+                                    readOnly={true}
+                                    disabled = {answered}
+                                />
+                                <span className="no-padding" />
+                            </label>
+                        </span>
+                    </div>
+                    {answered && realAnswer}
                 </div>
 
             </div>
         </div>
     );
 };
+
+export default Choice;
--- src/pages/Quiz.js
+++ src/pages/Quiz.js
@@ -1,7 +1,52 @@
-import React from "react";
+//import React from "react";
 import {Link} from "react-router-dom";
+import axios from "axios";
+import React, {useEffect, useState} from "react";
+
+const url = "https://quizapi.io/api/v1/questions";
+
+// config for axios
+const config = {
+    params:{
+        limit: 1,
+    },
+    headers:{
+        "X-Api-Key": process.env.REACT_APP_QUIZAPI_KEY,
+    },
+};
 
 const Quiz = ()=>{
+    const [quizzes, setquizzes] = useState(null);
+    const [isLoading, setLoading] = useState(true);
+    const [selectedAnswers, setSelectedAnswers] = useState([]);
+    const [isCorrect, setCorrect] = useState(null);
+    const [answered, setAnswered] = useState(false);
+        
+    useEffect(() => {
+        fetchQuiz();
+        return;
+    },[]);
+
+    const fetchQuiz = () => {
+      setAnswered(false);
+      setLoading(true);
+      setSelectedAnswers([]);
+      axios
+        .get(url, config) 
+        .then((res) => {
+        // console.log(res) 
+        if (res.data) {
+            setquizzes(res.data[0]); }
+        })
+        .catch((e) => {
+            alert("エラーだよ。コンソールを見てね");
+            console.error(e); 
+         })
+        .finally(() => { 
+            setLoading(false);
+    }); 
+};
+
     return(
         <div>
             <h2>クイズです</h2>
